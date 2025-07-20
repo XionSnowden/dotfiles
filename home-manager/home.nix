c873c91edc7b407f43b80c42e2cc6b24b8a727fe
Xion Snowden <xion3.snowden@gmail.com>
2025-07-20 10:25:55 -0400

    more

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,26 +6,27 @@
   home.username = "xion";
   home.homeDirectory = "/home/xion";
 
-  #Don't change
+  # Don't change
   home.stateVersion = "24.11";
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-     wofi
-     hyprpaper
-     waybar
-     xfce.thunar
-     kitty
-     librewolf-bin
-     neovim
-     fish
-     vesktop
-     spotify
-     vimPlugins.lazy-nvim
-     nodejs_24
-     kdePackages.kdeconnect-kde
-    
+    wofi
+    hyprpaper
+    waybar
+    xfce.thunar
+    kitty
+    librewolf-bin
+    neovim
+    fish
+    vesktop
+    spotify
+    # vimPlugins.lazy-nvim # This is likely incorrect. lazy-nvim is a Neovim plugin manager, not a package itself.
+                          # You'd manage Neovim plugins differently, often within your Neovim configuration.
+    nodejs_24
+    kdePackages.kdeconnect-kde
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -40,26 +41,32 @@
     # '')
   ];
 
-   xdg.userDirs = {
+  xdg.userDirs = {
     enable = true;
     createDirectories = true;
   };
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-
   home.file = {
+    # The .source attribute expects a path, so you need to provide it as a string.
+    # Also, using `home.path` or `lib.file.mkOutOfStoreSymlink` is often preferred
+    # for linking files outside the Nix store, but for direct dotfile management,
+    # the string path is correct.
 
-     ".config/nvim.source" = ~/dotfiles/nvim;
-     ".config/fish.source" = ~/dotfiles/fish;
-     ".config/hypr.source" = ~/dotfiles/hypr;
-     ".config/wofi.source" = ~/dotfiles/wofi;
-     ".config/waybar.source" = ~/dotfiles/waybar;
-     ".config/kitty.source" = ~/dotfiles/kitty;
-     "Pictures/Wallpapers.source" = ~/dotfiles/Wallpapers;
-     "Documents/Projects.source" = ~/dotfiles/Projects;
+    # These are symlinks. Home Manager will create symlinks from the target to the source.
+    ".config/nvim" = { source = "${config.home.homeDirectory}/dotfiles/nvim"; recursive = true; };
+    ".config/fish" = { source = "${config.home.homeDirectory}/dotfiles/fish"; recursive = true; };
+    ".config/hypr" = { source = "${config.home.homeDirectory}/dotfiles/hypr"; recursive = true; };
+    ".config/wofi" = { source = "${config.home.homeDirectory}/dotfiles/wofi"; recursive = true; };
+    ".config/waybar" = { source = "${config.home.homeDirectory}/dotfiles/waybar"; recursive = true; };
+    ".config/kitty" = { source = "${config.home.homeDirectory}/dotfiles/kitty"; recursive = true; };
+    "Pictures/Wallpapers" = { source = "${config.home.homeDirectory}/dotfiles/Wallpapers"; recursive = true; };
+    "Documents/Projects" = { source = "${config.home.homeDirectory}/dotfiles/Projects"; recursive = true; };
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
+    # # symlink to the # Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
 
     # # You can also set the file content immediately.
@@ -69,23 +76,21 @@
     # '';
   };
 
-  
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
   # through Home Manager then you have to manually source 'hm-session-vars.sh'
   # located at either
   #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #   ~/.nix-profile/etc/profile.d/hm-session-vars.sh
   #
   # or
   #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #   ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
   #
   # or
   #
-  #  /etc/profiles/per-user/xion/etc/profile.d/hm-session-vars.sh
+  #   /etc/profiles/per-user/xion/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
     # EDITOR = "emacs";
@@ -93,6 +98,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  };
-
 }
