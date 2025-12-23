4ff73ed3c7e4e4f72b95baa6a75f5d2f8af4dec6
XionSnowden <xion3.snowden@gmail.com>
2025-12-23 02:13:44 +0000

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,141 +1,64 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "xion";
   home.homeDirectory = "/home/xion";
-
-  # Don't change
   home.stateVersion = "24.11";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
- # 1. User Packages
   home.packages = with pkgs; [
     # --- Desktop & GUI Apps ---
-    hyprpaper
-    waybar
-    xfce.thunar
-    kitty
-    ghostty      # Ensure you are on unstable or have the proper overlay
-    librewolf-bin
-    neovim
-    vesktop
-    spotify
-    wlogout
-    cava
-    pavucontrol
-    blueberry
-    yad
+    hyprpaper waybar xfce.thunar kitty ghostty firefox
+    neovim vesktop spotify wlogout cava pavucontrol blueberry yad
+    nodejs_24 kdePackages.kdeconnect-kde
 
     # --- CLI Productivity ---
-    fish
-    starship
-    axel
-    bc
-    coreutils
-    curl
-    wget
-    rsync
-    ripgrep
-    jq
-    playerctl
-    cliphist
-    wl-clipboard
-    hyprpicker
-    tesseract
+    fish starship axel bc coreutils curl wget rsync ripgrep jq
+    playerctl cliphist wl-clipboard hyprpicker tesseract
 
     # --- Development & Build Tools ---
-    cmake
-    meson
-    npm
-    typescript
-    gjs
-    gobject-introspection
-    glib
-    tinyxml2
-    dart-sass
+    cmake meson npm typescript gjs gobject-introspection glib
+    tinyxml2 dart-sass
 
     # --- Wayland / Graphics Utilities ---
-    rofi-wayland
-    brightnessctl
-    swww
-    grim
-    swappy
-    wf-recorder
-    gradience
-    matugen
-    webp-pixbuf-loader
+    rofi-wayland brightnessctl swww grim swappy wf-recorder gradience matugen webp-pixbuf-loader
 
     # --- Libraries & Theming ---
-    gtk3
-    gtk-layer-shell
-    libdbusmenu-gtk3
-    libpulse
-    libsoup_3
-    libnotify
-    gtkmm3
-    gtksourceview3
-    gtksourceviewmm
-    cairomm
-    qt5ct
-    qt5.qtwayland
-    adw-gtk3
-    bibata-cursors
+    gtk3 gtk-layer-shell libdbusmenu-gtk3 libpulse libsoup_3 libnotify
+    gtkmm3 gtksourceview3 gtksourceviewmm cairomm qt5ct qt5.qtwayland
+    adw-gtk3 bibata-cursors
 
     # --- Fonts ---
-    readex-pro
-    gabarito
-    rubik
-    material-symbols
-    noto-fonts-emoji
-    noto-fonts-cjk-sans
+    readex-pro gabarito rubik material-symbols noto-fonts-emoji noto-fonts-cjk-sans
     (nerdfonts.override { fonts = [ "JetBrainsMono" "SpaceMono" ]; })
 
     # --- Python Environment ---
     (python3.withPackages (ps: with ps; [
-      libsass
-      pywalfox
-      pywal
-      pillow
-      build
-      setuptools-scm
-      wheel
-      pywayland
-      psutil
-      metar
+      libsass pywalfox pywal pillow build setuptools-scm
+      wheel pywayland psutil metar
     ]))
   ];
 
-  # 2. Program Modules
   programs.foot = {
     enable = true;
-    settings = {
-      main = { font = "monospace:size=11"; };
-    };
+    settings.main.font = "monospace:size=11";
   };
 
   programs.starship.enable = true;
+  programs.home-manager.enable = true;
 
   programs.hyprlock = {
     enable = true;
     settings = {
-      general = {
-        disable_loading = true;
-        grace = 0;
-      };
+      general = { disable_loading = true; grace = 0; };
       background = [{
-        path = "/home/user/wallpaper.png"; # Ensure this file exists!
+        path = "/home/xion/wallpaper.png"; # Fixed path from /home/user/
         color = "rgba(25, 20, 20, 1.0)";
         blur_passes = 2;
       }];
     };
   };
 
-  # 3. Services
   services.gnome-keyring.enable = true;
-
   services.gammastep = {
     enable = true;
     provider = "geoclue2";
@@ -145,18 +68,11 @@
   services.hypridle = {
     enable = true;
     settings = {
-      general = {
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-        ignore_dbus_inhibit = false;
-      };
-      listener = [{
-        timeout = 300;
-        on-timeout = "hyprlock";
-      }];
+      general = { after_sleep_cmd = "hyprctl dispatch dpms on"; ignore_dbus_inhibit = false; };
+      listener = [{ timeout = 300; on-timeout = "hyprlock"; }];
     };
   };
 
-  # Polkit Agent (Required for elevated GUI tasks)
   systemd.user.services.polkit-gnome-authentication-agent-1 = {
     Unit = {
       Description = "polkit-gnome-authentication-agent-1";
@@ -173,7 +89,6 @@
     Install.WantedBy = [ "graphical-session.target" ];
   };
 
-  # 4. Global Theming & Config
   fonts.fontconfig.enable = true;
 
   home.pointerCursor = {
@@ -186,10 +101,7 @@
 
   gtk = {
     enable = true;
-    theme = {
-      name = "adw-gtk3";
-      package = pkgs.adw-gtk3;
-    };
+    theme = { name = "adw-gtk3"; package = pkgs.adw-gtk3; };
   };
 
   qt = {
@@ -197,80 +109,20 @@
     platformTheme.name = "qt5ct";
     style.name = "adwaita-dark";
   };
-}
-                  
-    # vimPlugins.lazy-nvim # This is likely incorrect. lazy-nvim is a Neovim plugin manager, not a package itself.
-                          # You'd manage Neovim plugins differently, often within your Neovim configuration.
-    nodejs_24
-    kdePackages.kdeconnect-kde
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
 
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # The .source attribute expects a path, so you need to provide it as a string.
-    # Also, using `home.path` or `lib.file.mkOutOfStoreSymlink` is often preferred
-    # for linking files outside the Nix store, but for direct dotfile management,
-    # the string path is correct.
-
-    # These are symlinks. Home Manager will create symlinks from the target to the source.
-
-    
-    
-    ".config/nvim" = { source = config.lib.file.mkOutOfStoreSymlink "home/xion/dotfiles/nvim"; recursive = true; };
-    ".config/kitty" = { source = config.lib.file.mkOutOfStoreSymlink "home/xion/dotfiles/kitty"; recursive = true; };
-    ".config/waybar" = { source = config.lib.file.mkOutOfStoreSymlink "home/xion/dotfiles/waybar"; recursive = true; };
-    "Pictures/Wallpapers" = { source = config.lib.file.mkOutOfStoreSymlink "home/xion/dotfiles/Wallpapers"; recursive = true; };
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the # Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/xion/dotfiles/nvim";
+    ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "/home/xion/dotfiles/kitty";
+    ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "/home/xion/dotfiles/waybar";
+    "Pictures/Wallpapers".source = config.lib.file.mkOutOfStoreSymlink "/home/xion/dotfiles/Wallpapers";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #   ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #   ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #   /etc/profiles/per-user/xion/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    # EDITOR = "neovim";
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
