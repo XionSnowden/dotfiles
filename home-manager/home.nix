2116c82ab55a50f49142858d13ed9a98b1e166d0
XionSnowden <xion3.snowden@gmail.com>
2026-03-27 01:42:56 -0400

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,125 +1,96 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
   home.username = "xion";
   home.homeDirectory = "/home/xion";
-  home.stateVersion = "24.11";
 
-  home.packages = with pkgs; [
-    # --- Desktop & GUI Apps ---
-    kitty ghostty firefox
-    neovim vesktop spotify wlogout cava pavucontrol blueberry yad
-    nodejs_24 kdePackages.kdeconnect-kde
-
-    # --- CLI Productivity ---
-    fish starship axel bc coreutils curl wget rsync ripgrep jq
-    playerctl cliphist wl-clipboard hyprpicker tesseract
-
-    # --- Development & Build Tools ---
-    cmake meson typescript gjs gobject-introspection glib
-    tinyxml2 dart-sass
-
-    # --- Wayland / Graphics Utilities ---
-    rofi-wayland brightnessctl swww grim swappy wf-recorder gradience matugen webp-pixbuf-loader
-
-    # --- Libraries & Theming ---
-    gtk3 gtk-layer-shell libdbusmenu-gtk3 libpulseaudio libsoup_3 libnotify
-    gtkmm3 gtksourceview3 gtksourceviewmm cairomm libsForQt5.qt5ct qt5.qtwayland
-    adw-gtk3 bibata-cursors
-
-    # --- Fonts ---
-    rubik material-symbols noto-fonts-emoji noto-fonts-cjk-sans
-    nerd-fonts.jetbrains-mono nerd-fonts.space-mono
-
-    # --- Python Environment ---
-    (python3.withPackages (ps: with ps; [
-      libsass pywalfox-native pywal pillow build setuptools-scm
-      wheel pywayland psutil metar
-    ]))
+  # Don't change
+  home.stateVersion = "25.11";
+  
+  #Allow specific unfree packages
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "unityhub"
+    "spotify"
+    "vscode"
   ];
 
-  programs.foot = {
-    enable = true;
-    settings.main.font = "monospace:size=11";
-  };
-
-  programs.starship.enable = true;
-  programs.home-manager.enable = true;
-
-  programs.hyprlock = {
-    enable = true;
-    settings = {
-      general = { disable_loading = true; grace = 0; };
-      background = [{
-        path = "/home/xion/wallpaper.png"; # Fixed path from /home/user/
-        color = "rgba(25, 20, 20, 1.0)";
-        blur_passes = 2;
-      }];
-    };
-  };
-
-  services.gnome-keyring.enable = true;
-  services.gammastep = {
-    enable = true;
-    provider = "geoclue2";
-    temperature.night = 3500;
-  };
-
-  services.hypridle = {
-    enable = true;
-    settings = {
-      general = { after_sleep_cmd = "hyprctl dispatch dpms on"; ignore_dbus_inhibit = false; };
-      listener = [{ timeout = 300; on-timeout = "hyprlock"; }];
-    };
-  };
-
-  systemd.user.services.polkit-gnome-authentication-agent-1 = {
-    Unit = {
-      Description = "polkit-gnome-authentication-agent-1";
-      Wants = [ "graphical-session.target" ];
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      Type = "simple";
-      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-    Install.WantedBy = [ "graphical-session.target" ];
-  };
-
-  fonts.fontconfig.enable = true;
-
-  home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 24;
-  };
-
-  gtk = {
-    enable = true;
-    theme = { name = "adw-gtk3"; package = pkgs.adw-gtk3; };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme.name = "qt5ct";
-    style.name = "adwaita-dark";
-  };
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    xfce.thunar
+    kitty
+    firefox
+    fish
+    vesktop
+    unityhub
+    spotify
+    gnome-extension-manager
+    nodejs_24
+    kdePackages.kdeconnect-kde
+    neovim
+    vscode
+    dotnetCorePackages.sdk_9_0_1xx-bin
+    obs-studio
+    openshot-qt
+    blender
+    unzip
+  ];
 
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
+    setSessionVariables = false;
   };
 
+
+  programs.vscode.userSettings = {
+  "dotnetAcquisitionExtension.sharedExistingDotnetPath" = "/home/xion/.nix-profile/bin/dotnet";
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
   home.file = {
-    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/xion/dotfiles/nvim";
+    # The .source attribute expects a path, so you need to provide it as a string.
+    # Also, using `home.path` or `lib.file.mkOutOfStoreSymlink` is often preferred
+    # for linking files outside the Nix store, but for direct dotfile management,
+    # the string path is correct.
+
+    # These are symlinks. Home Manager will create symlinks from the target to the source.
+
+   # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the # Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
   };
 
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #   ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #   ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #   /etc/profiles/per-user/xion/etc/profile.d/hm-session-vars.sh
+  #
   home.sessionVariables = {
-    # EDITOR = "neovim";
+    # EDITOR = "emacs";
   };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
